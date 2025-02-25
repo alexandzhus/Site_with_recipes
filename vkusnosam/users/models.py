b4c8419668d7a998d61ff9f5947ac18db7cfc359
vboxuser <alexandrdzhus@gmail.com>
2025-02-25 10:50:37 +0300

    Создал модель для приложения users.

--- vkusnosam/users/models.py
+++ vkusnosam/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """
+    Класс для расширения стандартной модели user
+    """
+    avatar = models.ImageField(blank=True, null=True, upload_to='users/%Y/%m/%d', verbose_name='Аватар')
+    age = models.PositiveIntegerField(blank=True, null=True, verbose_name='Возраст')
+    interests = models.TextField(blank=True, null=True, verbose_name='Интересы')
+
+
