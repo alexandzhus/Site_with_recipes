33d92bf67fbc89ea2b39f9fdf79133b71751c522
vboxuser <alexandrdzhus@gmail.com>
2025-02-25 10:54:26 +0300

    Добавил пути для проекта и приложений.

--- vkusnosam/recipes/urls.py
+++ vkusnosam/recipes/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import test_index_page
+from .views import HomeView, RecipeDetail, CreateRecipe, UpdateRecipe, DeleteRecipe, TagCloudByCategoryView, \
+    TaggedRecipesView
 
 urlpatterns = [
-    path('', test_index_page, name='index'),
+    path('', HomeView.as_view(), name='home'),
+    path('recipe_detail/<slug:recipe_slug>/', RecipeDetail.as_view(), name='recipe_detail'),
+
+    path('tag-cloud/', TagCloudByCategoryView.as_view(), name='tag_cloud_by_category'),
+    path('tag-cloud/<slug:tag_category_slug>/', TagCloudByCategoryView.as_view(), name='tag_cloud_by_category'),
+    path('tag/<str:tag_slug>/', TaggedRecipesView.as_view(), name='tagged_recipes'),
+
+    path('create_recipe/', CreateRecipe.as_view(), name='create_recipe'),
+    path('update_recipe/<slug:slug>/', UpdateRecipe.as_view(), name='update_recipe'),
+    path('delete_recipe/<slug:slug>/', DeleteRecipe.as_view(), name='delete_recipe'),
 ]
--- vkusnosam/users/urls.py
+++ vkusnosam/users/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.contrib.gis.gdal.prototypes.srs import from_user_input
+from django.urls import path, reverse_lazy
+
+from .views import CustomLoginView, RegisterView, ProfileView
 
 app_name = 'users'
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+
+    # Сброс пароля
 
 ]
