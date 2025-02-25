0b99320b97ff04ad3ef9a679caa90517b4367bae
vboxuser <alexandrdzhus@gmail.com>
2025-02-25 10:45:43 +0300

    Админка для приложения users.

--- vkusnosam/users/admin.py
+++ vkusnosam/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from users.models import User
+
+
+
+@admin.register(User)
+class UsersAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'password', 'email', 'first_name', 'last_name', "avatar", 'age', 'interests')
+    list_display_links = ('username', 'password', 'email', 'first_name', 'last_name', "avatar", 'age', 'interests')
